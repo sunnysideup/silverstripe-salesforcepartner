a1684d834c7bd422ba71cc50b871eed13b478e0f
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2019-07-13 07:55:42 +1200

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/Model/SalesforceDefaultContactField.php
+++ code/Model/SalesforceDefaultContactField.php
@@ -29,6 +29,20 @@ class SalesforceDefaultContactField extends DataObject
      *
      * @var array
      */
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'SalesforceDefaultContactField';
+
     private static $db = [
         'Key' => 'Varchar',
         'Value' => 'Varchar',
